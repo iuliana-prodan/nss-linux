a64697a14816c0e60c17a76ed8fdcc308f4401ae
Iuliana Prodan <iuliana.prodan@nxp.com>
2024-07-10 14:25:16 +0300

    lab2

--- drivers/nss/lab02/ex2/my-awesome-driver.c
+++ drivers/nss/lab02/ex2/my-awesome-driver.c
@@ -8,8 +8,49 @@
 #include <linux/platform_device.h>
 
 /* TODO1: declare the struct of_device_id array */
+static const struct of_device_id my_awesome_driver_ids[] = {
+	{ .compatible = "nss,my-awesome-device", },
+	{ },
+};
+
+static int my_probe(struct platform_device *pdev)
+{
+	printk("Hello World\n");
+	return 0;
+}
+
 /* TODO2: create the platform driver structure */
+static struct platform_driver my_awesome_driver = {
+	.probe		= my_probe,
+	.driver = {
+		.name	= "my_awesome_driver",
+		.of_match_table = my_awesome_driver_ids,
+	},
+};
+
 /* TODO3: create the init/exit functions */
+static int __init my_init(void)
+{
+	int ret;
+
+	ret = platform_driver_register(&my_awesome_driver);
+	if (ret)
+		printk("error\n");
+	else
+		printk("ok\n");
+
+	return ret;
+}
+
+static void __exit my_exit(void)
+{
+	platform_driver_unregister(&my_awesome_driver);
+}
+
+module_platform_driver(my_awesome_driver);
+
+//module_init(my_init);
+//module_exit(my_exit);
 
 MODULE_DESCRIPTION("My very awesome platform driver");
 MODULE_AUTHOR("Facultatea de Vrajitorie si Farmece");
--- drivers/nss/lab02/ex3/led.c
+++ drivers/nss/lab02/ex3/led.c
@@ -14,12 +14,28 @@ struct led_data {
 	struct cdev cdev;
 };
 
+struct led_data *my_led;
+
+static ssize_t	led_read(struct file *, char *, size_t, loff_t *);
+static ssize_t	led_write(struct file *, const char *, size_t, loff_t *);
+
+static const struct file_operations led_fops = {
+	.owner		= THIS_MODULE,
+//	.open		= led_open,
+	.read		= led_read,
+	.write		= led_write
+};
+
 static void led_remove(struct platform_device *pdev)
 {
 	struct led_data *data = platform_get_drvdata(pdev);
+	dev_t devno;
 
 	/* TODO: delete chardev */
 	/* TODO: unregister chardev region */
+	devno = MKDEV(500, 0);
+	unregister_chrdev_region(devno, 1);
+	cdev_del(&data->cdev);
 }
 
 static int led_probe(struct platform_device *pdev)
@@ -27,6 +43,7 @@ static int led_probe(struct platform_device *pdev)
 	struct led_data *data;
 	struct device *dev;
 
+	dev_t devno;
 	dev = &pdev->dev;
 
 	dev_info(dev, "LED driver probe start\n");
@@ -37,13 +54,64 @@ static int led_probe(struct platform_device *pdev)
 		return -ENOMEM;
 	}
 
+	devno = MKDEV(500, 0);
+
 	/* TODO: get GPIO handle */
+	data->led = devm_gpiod_get(&pdev->dev, "led", GPIOD_OUT_LOW);
+	if (IS_ERR(data->led)) {
+	    dev_err(dev, "LED error\n");
+	}
+
 	/* TODO: register chardev region */
+	register_chrdev_region(devno, 1 , "led");
+
 	/* TODO: initialize and add chardev */
+	cdev_init(&data->cdev, &led_fops);
+	data->cdev.owner = THIS_MODULE;
+	cdev_add(&data->cdev, devno, 1);
 
 	platform_set_drvdata(pdev, data);
 
+	my_led = data;
+
 	dev_info(dev, "LED driver probe end\n");
+
+	return 0;
+}
+
+static ssize_t led_read(struct file *filep, char __user *buffer, size_t len, loff_t *offset)
+{
+    int val_ret = 0;
+    int led = 0;
+    char *msg;
+
+    len = min(len, strlen(msg) + 1);
+
+    led = gpiod_get_value(my_led->led);
+    if (led == 1)
+	msg = "on";
+    else if (led == 0)
+	msg = "off";
+    else
+	msg = "err";
+
+    val_ret = copy_to_user(buffer, msg, len);
+    if (val_ret == 0)
+	return len;
+    else
+	return len - val_ret;
+}
+
+static ssize_t led_write(struct file *filep, const char *buffer, size_t len, loff_t *offset)
+{
+	pr_info("buffer = %s\n", buffer);
+
+	if(!strcmp(buffer, "on"))
+	    gpiod_set_value(my_led->led, 1);
+	else if (!strcmp(buffer, "off"))
+	    gpiod_set_value(my_led->led, 0);
+	else
+	    printk("Invalid value from user\n");
 
 	return 0;
 }
